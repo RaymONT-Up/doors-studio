d032ab2720ee23042a7613b4e588b56930a03efa
Raym0NT <konasr6@gmail.com>
2023-04-30 19:34:03 +0300

    space-for designers slider mobile v

--- js/space-for-designers.js
+++ js/space-for-designers.js
@@ -1,27 +1,79 @@
 const spaceForDesignersPagination = document.querySelector(
   ".space-for-designers__pagination"
 );
+const sliderItem = document.querySelector(".space-for-designers__item");
+
 const spaceForDesignersSlider = new Swiper(".space-for-designers__slider", {
-  slidesPerView: "auto",
+  slidesPerView: 5,
 
   spaceBetween: 30,
-
   navigation: {
     nextEl: ".space-for-designers__slider-arrow--next",
     prevEl: ".space-for-designers__slider-arrow--prev",
   },
+  slidesOffsetAfter: 1,
+
   on: {
     init: function () {
-      const activeIndex = 4 + this.activeIndex;
-      const totalSlides = this.slides.length;
+      if (window.innerWidth > 600) {
+        const activeIndex = 4 + this.activeIndex;
+        const totalSlides = this.slides.length;
+        spaceForDesignersPagination.textContent = `${activeIndex} из ${totalSlides} фото`;
 
-      spaceForDesignersPagination.textContent = `${activeIndex} из ${totalSlides} фото`;
+        // Получаю ширину и маргин элемента дабы задать отступ в слайдере(без этого последний слайд не будет видно)
+        // После эо значение записывается в свойство объекта swiper в виде offset
+        const sliderItemWidth = parseFloat(
+          sliderItem.style.cssText.split(";")[0].split(" ")[1]
+        );
+        const sliderItemMargin = parseFloat(
+          sliderItem.style.cssText.split(";")[0].split(" ")[1]
+        );
+        const offset = (sliderItemWidth + sliderItemMargin) * 2;
+        this.params.slidesOffsetAfter = offset;
+        this.update();
+      } else {
+        const activeIndex = 1 + this.activeIndex;
+        const totalSlides = this.slides.length;
+        spaceForDesignersPagination.textContent = `${activeIndex} из ${totalSlides} фото`;
+      }
     },
     slideChange: function () {
-      const activeIndex = 4 + this.activeIndex;
-      const totalSlides = this.slides.length;
+      if (window.innerWidth > 600) {
+        const activeIndex = 4 + this.activeIndex;
+        const totalSlides = this.slides.length;
+        spaceForDesignersPagination.textContent = `${activeIndex} из ${totalSlides} фото`;
 
-      spaceForDesignersPagination.textContent = `${activeIndex} из ${totalSlides} фото`;
+        // Запрет слайда когда мы подходим к последнему с конца -3 слайду
+        if (activeIndex === totalSlides + 1) {
+          this.slidePrev();
+          return;
+        }
+      } else {
+        const activeIndex = 1 + this.activeIndex;
+        const totalSlides = this.slides.length;
+
+        spaceForDesignersPagination.textContent = `${activeIndex} из ${totalSlides} фото`;
+      }
     },
+  },
+
+  breakpoints: {
+    0: {
+      spaceBetween: 10,
+      slidesPerView: 1.15,
+      centeredSlides: true,
+    },
+    601: {
+      spaceBetween: 10,
+    },
+    700: {
+      spaceBetween: 20,
+      slidesPerView: 5.5,
+    },
+    800: {
+      spaceBetween: 30,
+      slidesPerView: 5,
+    },
+    1000: {},
   },
 });
