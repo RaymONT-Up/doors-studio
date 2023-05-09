dc863b7cb76a844714ac06655388a1fd0be23ba0
Raym0NT <konasr6@gmail.com>
2023-05-09 21:02:32 +0300

    coworking-for, coworking-about,coworking-hero tablet v

--- js/coworking-reviews.js
+++ js/coworking-reviews.js
@@ -32,40 +32,6 @@ const coworkingReviewsImgSlider = new Swiper(".coworking-reviews__slider", {
 
   // Выключение перетаскивания мышью
   allowTouchMove: false,
-
-  // on: {
-  //   // init: function () {
-  //   //   coworkingRewiewsTextList.forEach(item => {
-  //   //     item.classList.remove("coworking-reviews__item-review--active");
-  //   //   });
-  //   //   coworkingRewiewsTextList[this.activeIndex].classList.add(
-  //   //     "coworking-reviews__item-review--active"
-  //   //   );
-  //   //   console.log(this.activeIndex, coworkingRewiewsTextList[this.activeIndex]);
-  //   // },
-  //   // slideChange: function () {
-  //   //   coworkingRewiewsTextList.forEach(item => {
-  //   //     item.classList.remove("coworking-reviews__item-review--active");
-  //   //   });
-  //   //   coworkingRewiewsTextList[this.activeIndex].classList.add(
-  //   //     "coworking-reviews__item-review--active"
-  //   //   );
-  //   //   console.log(this.activeIndex, coworkingRewiewsTextList[this.activeIndex]);
-  //   // },
-  // },
-
-  on: {
-    // init: function () {
-    //   console.log(this.activeIndex, this);
-    //   const activeIndex = this.activeIndex;
-    //   const slides = this.slides;
-    //   console.log(slides);
-    //   slides[activeIndex].classList.remove("swiper-slide-active");
-    //   slides[activeIndex + 1].classList.remove("swiper-slide-next");
-    //   slides[activeIndex + 1].classList.add("swiper-slide-active1");
-    //   slides[activeIndex + 2].classList.add("swiper-slide-next");
-    // },
-  },
 });
 
 coworkingReviewsImgSlider.controller.control = coworkingReviewSlider;
