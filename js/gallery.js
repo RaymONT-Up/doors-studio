527441e950978fc4c8631f01e19af5f19a21ed62
Raym0NT <konasr6@gmail.com>
2023-05-11 22:21:37 +0300

    little fixs and gallery update

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,19 @@
 Fancybox.bind('[data-fancybox="gallery"]', {
+  idle: false,
+
+  Thumbs: {
+    type: false,
+  },
+
   Toolbar: {
+    items: {
+      btnCloseCustom: {
+        tpl: `<button data-fancybox-close class="gallery__close-btn  f-button is-close-btn" title="{{CLOSE}}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" tabindex="-1"><path d="M20 20L4 4m16 0L4 20"/></svg></button>`,
+      },
+    },
     display: {
       left: ["infobar"],
-      right: ["close"],
-      bottom: ["infobar"],
+      right: ["btnCloseCustom"],
     },
-  },
-  caption: (fancybox, slide) => {
-    return `${slide.index + 1} / ${fancybox.carousel?.slides.length}`;
   },
 });
