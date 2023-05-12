8297ce185a7c9f1804d52e1afbcd9338bdccff16
Raym0NT <konasr6@gmail.com>
2023-05-12 21:38:38 +0300

    we-in-touch contact block update with state for inputs

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,7 @@ let popup = document.querySelectorAll(".popup");
 let popupClose = document.querySelectorAll(".popupClose");
 let popupOverlay = document.querySelectorAll(".popupOverlay");
 let popupWindow = document.querySelectorAll(".popupWindow");
+let popupLink = document.querySelectorAll('[data-popup="pre-order]');
 
 if (popup) {
   const closePopup = (el, timer = 0) => {
@@ -17,3 +18,13 @@ if (popup) {
   closePopup(popupOverlay);
   closePopup(popupClose);
 }
+
+// const activePopup = el => {
+//   let popupActivated = document.querySelector(el);
+//   el.classList.add("active");
+// };
+// if (popupLink) {
+//   for (elem of popupLink) {
+//     elem.addEventListener("click", activePopup);
+//   }
+// }
